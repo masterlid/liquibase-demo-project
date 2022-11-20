ea81a70a00582f401f87306504ae8c49dcc93f9a
Master Lid <master@lid.tv>
2022-11-20 16:47:52 +0300

    Initial Java source code commit

---
+++ src/main/java/tv/lid/jen/liquibase/LiquibaseDemoProject/LiquibaseDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package tv.lid.jen.liquibase.LiquibaseDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tv/lid/jen/liquibase/LiquibaseDemoProject/LiquibaseDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.lid.jen.liquibase.LiquibaseDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
